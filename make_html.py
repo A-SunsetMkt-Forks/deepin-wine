b75581171b8cec282f963231fbafdca0c8fdd89b
张强 <email2zq@qq.com>
2025-01-06 19:24:38 +0800

    升级至deepin-v23

--- make_html.py
+++ make_html.py
@@ -1,5 +1,6 @@
 import html
 import datetime
+import pytz
 
 import repo
 
@@ -26,5 +27,5 @@ with open(OUTPUT, 'wt') as f:
     f.write(before_t)
     for x in result:
         f.write(the_t % tuple(map(html.escape, x)))
-    update_time = (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime('%Y-%m-%d %H:%M:%S')
+    update_time = datetime.datetime.now(pytz.timezone('Asia/Shanghai')).isoformat()
     f.write(after_t.replace('<!--update_time-->', update_time))
